b5f4fca9729550b02c65d4891d6e570bd69bb774
KillWolfVlad <KillWolfVlad@users.noreply.github.com>
2026-05-19 18:36:09 +0500

    fix: fix useScrollLock

--- src/hooks/useScrollLock.ts
+++ src/hooks/useScrollLock.ts
@@ -9,20 +9,25 @@ export function useScrollLock(locked: boolean) {
     if (!locked) return;
 
     const scrollbarWidth = getScrollbarWidth();
-    if (scrollbarWidth === 0) return;
 
     const prevOverflow = document.body.style.overflow;
-    const prevPaddingRight = document.body.style.paddingRight;
-    const bodyComputedPadding = parseFloat(
-      getComputedStyle(document.body).paddingRight,
-    );
-
     document.body.style.overflow = "hidden";
-    document.body.style.paddingRight = `${bodyComputedPadding + scrollbarWidth}px`;
+
+    if (scrollbarWidth > 0) {
+      const prevPaddingRight = document.body.style.paddingRight;
+      const bodyComputedPadding = parseFloat(
+        getComputedStyle(document.body).paddingRight,
+      );
+      document.body.style.paddingRight = `${bodyComputedPadding + scrollbarWidth}px`;
+
+      return () => {
+        document.body.style.overflow = prevOverflow;
+        document.body.style.paddingRight = prevPaddingRight;
+      };
+    }
 
     return () => {
       document.body.style.overflow = prevOverflow;
-      document.body.style.paddingRight = prevPaddingRight;
     };
   }, [locked]);
 }
